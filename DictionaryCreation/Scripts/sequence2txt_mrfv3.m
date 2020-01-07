3c58f4ba2b8bb59f210c891b55e40da9f182595c
Aurélien <aurelien.delphin@gmail.com>
2020-01-07 17:21:53 +0100

    Turned this script into a function to avoid global variables

--- DictionaryCreation/Scripts/sequence2txt_mrfv3.m
+++ DictionaryCreation/Scripts/sequence2txt_mrfv3.m
@@ -1,10 +1,11 @@
+function [] = sequence2txt_mrfv3(rootDir, name, Sequence)
 % %% Creation of the directory
-% dossier=strcat('D:\MATLAB\MRF_v2\Data\Séquences_et_dicos\',date);
+% dossier=strcat('D:\MATLAB\MRF_v2\Data\Sï¿½quences_et_dicos\',date);
 % mkdir(dossier);
 % cd(dossier);
 
 %% Creation of the text file
-fid = fopen(strcat(name,'.txt'),'w');
+fid = fopen(strcat(rootDir, 'SequencesAndProperties/TextSequences/', name,'.txt'),'w');
 
 
 % Writing the sequence
@@ -23,12 +24,13 @@ fprintf(fid,'%d\n',Sequence.Ncycles);
 % end
 
 for k=1:Sequence.Npulses
-fprintf(fid,'%+f %f %f\n',(Sequence.FA(k))*180/pi,Sequence.TR(k),Sequence.TE(k)); % Car FA en rad dans cette simu et a priori alternance +/- mais pas écrit dans le vecteur FA
-
-
+    fprintf(fid,'%+f %f %f\n',(Sequence.FA(k))*180/pi,Sequence.TR(k),Sequence.TE(k)); % Car FA en rad dans cette simu et a priori alternance +/- mais pas ï¿½crit dans le vecteur FA
+    
+    
 end
 
 
 fclose(fid);
 
 clear dossier fid k ans
+end
