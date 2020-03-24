2b552ccef774d47b2db52cc223d6f082f72809aa
Aurélien <aurelien.delphin@gmail.com>
2020-03-24 11:35:30 +0100

    Turned scripts into functions

--- ResultAnalysis/scripts/dcm2matrix_MRFv3.m
+++ ResultAnalysis/scripts/dcm2matrix_MRFv3.m
@@ -1,58 +1,96 @@
-Images.dossier=strcat(Images.dossier_visu,'/dicom/');
-if Images.Nimages>999
-    X=dicomread(strcat(Images.dossier,'MRIm0001.dcm'));
-elseif Images.Nimages>99
-    X=dicomread(strcat(Images.dossier,'MRIm001.dcm'));
-elseif Images.Nimages>9
-    X=dicomread(strcat(Images.dossier,'MRIm01.dcm'));
+function Images = dcm2matrix_MRFv3(acqPath)
+
+pathPData = fullfile(acqPath, 'pdata', '1');
+pathDcm = fullfile(pathPData, 'dicom');
+Images=struct;
+%%%%%%%%%% Commented to try to find someting simpler
+
+% %% Get number of frames (time points)
+% Images.nImages = size(dictionary,2);
+% 
+% %% Open first image and get its size
+% Images.dossier=strcat(Images.dossier_visu,'/dicom/');
+% if Images.nImages>999
+%     X=dicomread(strcat(Images.dossier,'MRIm0001.dcm'));
+% elseif Images.nImages>99
+%     X=dicomread(strcat(Images.dossier,'MRIm001.dcm'));
+% elseif Images.nImages>9
+%     X=dicomread(strcat(Images.dossier,'MRIm01.dcm'));
+% else
+%     X=dicomread(strcat(Images.dossier,'MRIm1.dcm'));
+% end
+% 
+% [Images.nX,Images.nY]=size(X);
+% clear X;
+
+%% Open visuPars Bruker file and read it
+% visu_pars=fopen(strcat(Images.dossier_visu,'/visu_pars'),'r','ieee-le');
+fidVisuPars = fopen(fullfile(pathPData, 'visu_pars'), 'r','ieee-le');
+if fidVisuPars > 0 % if file opened
+    texteVisuPars = fread(fidVisuPars, inf, '*char');
+% texteVisuPars=char(fread(visu_pars,[inf],'int8'));
+    fclose(fidVisuPars);
+    texteVisuPars=texteVisuPars(:)';
 else
-    X=dicomread(strcat(Images.dossier,'MRIm1.dcm'));
+    error('Could not open file %s', fullfile(pathPData, 'visu_pars'))
 end
 
+%% Extract parameters
+Images.nImages = scan_acqp('##$VisuCoreFrameCount=',texteVisuPars,1); % nb of time points
+S = scan_acqp('##$VisuCoreSize=', texteVisuPars,1)'; % image size
+Images.nX = S(1); 
+Images.nY = S(2);
+VisuCoreDataSlope=scan_acqp('##$VisuCoreDataSlope=',texteVisuPars,1); %Needed to apply transform to data
 
-[Images.Nx,Images.Ny]=size(X);
-clear X;
-Images.Images_dicom=zeros(Images.Nx,Images.Ny,Images.Nimages);
-
-for i=1:Images.Nimages
-    if Images.Nimages>999
-        filename=sprintf('MRIm%04i.dcm',i);
-    elseif Images.Nimages>99
-        filename=sprintf('MRIm%03i.dcm',i);
-    elseif Images.Nimages>9
-        filename=sprintf('MRIm%02i.dcm',i);
+%% Open all images in order
+% Images.dossier=strcat(Images.dossier_visu,'/dicom/');
+Images.Images_dicom = zeros(Images.nX,Images.nY,Images.nImages);
+for i=1:Images.nImages
+    if Images.nImages>999
+        filename = sprintf('MRIm%04i.dcm',i);
+    elseif Images.nImages>99
+        filename = sprintf('MRIm%03i.dcm',i);
+    elseif Images.nImages>9
+        filename = sprintf('MRIm%02i.dcm',i);
     else
-        filename=sprintf('MRIm%01i.dcm',i);
+        filename = sprintf('MRIm%01i.dcm',i);
     end
-    Images.Images_dicom(:,:,i)=dicomread(strcat(Images.dossier,filename));
+    Images.Images_dicom(:,:,i) = dicomread(fullfile(pathDcm, filename));
 end
 
+%% Apply linear transformation
+% visu_pars=fopen(strcat(Images.dossier_visu,'/visu_pars'),'r','ieee-le');
+% texteVisuPars = fread(visu_pars, inf, '*char');
+% % texteVisuPars=char(fread(visu_pars,[inf],'int8'));
+% fclose(visu_pars);
+% texteVisuPars=texteVisuPars(:)';
+% clear ans visu_pars
+% % Images.VisuCoreDataMax=scan_acqp('##$VisuCoreDataMax=',texteVisuPars,1);
+% Images.VisuCoreDataSlope=scan_acqp('##$VisuCoreDataSlope=',texteVisuPars,1); %Needed to apply transform to data
+% clear texteVisuPars filename
 
-visu_pars=fopen(strcat(Images.dossier_visu,'/visu_pars'),'r','ieee-le');
-texteVisuPars=char(fread(visu_pars,[inf],'int8'));
-fclose(visu_pars);
-texteVisuPars=texteVisuPars(:)';
-clear ans visu_pars
- Images.VisuCoreDataMax=scan_acqp('##$VisuCoreDataMax=',texteVisuPars,1);
- Images.VisuCoreDataSlope=scan_acqp('##$VisuCoreDataSlope=',texteVisuPars,1);
-clear texteVisuPars filename
-
-for i=1:Images.Nimages
-    Images.Images_dicom_rescaled(:,:,i)=double(Images.Images_dicom(:,:,i))*Images.VisuCoreDataSlope(i);%/32767;%*Images.VisuCoreDataMax(i)
+for i=1:Images.nImages
+    Images.Images_dicom_rescaled(:,:,i)=double(Images.Images_dicom(:,:,i))*VisuCoreDataSlope(i);%/32767;%*Images.VisuCoreDataMax(i)
 end
-clear i% Images.VisuCoreDataMax Images.VisuCoreDataSlope
-clear Images.Images_dicom
+% clear i% Images.VisuCoreDataMax Images.VisuCoreDataSlope
+% clear Images.Images_dicom
 
-
-dossier_init=pwd;
-cd(Images.dossier_visu)
-cd ..
-cd ..
-method=fopen('method','r','ieee-le');
-texteMethod=char(fread(method,[inf],'int8'));
-fclose(method);
-texteMethod=texteMethod(:)';
-clear ans method
+%% Read method bruker file and get parameters
+% dossier_init=pwd;
+% cd(Images.dossier_visu)
+% cd ..
+% cd ..
+fidMethod=fopen(fullfile(acqPath, 'method'), 'r', 'ieee-le');
+if fidMethod > 0
+% texteMethod=char(fread(method,[inf],'int8'));
+    texteMethod=fread(fidMethod, inf, '*char');
+    fclose(fidMethod);
+    texteMethod=texteMethod(:)';
+else
+    error('Could not open file %s', fullfile(acpPath, 'method'))
+end
+% clear ans method
 Images.PVM_SliceThick=scan_acqp('##$PVM_SliceThick=',texteMethod,1);
-cd(dossier_init)
-clear Images.dossier_visu texteMethod dossier_init
+% cd(dossier_init)
+% clear Images.dossier_visu texteMethod dossier_init
+end
--- ResultAnalysis/scripts/fp_normalization_MRFv3.m
+++ ResultAnalysis/scripts/fp_normalization_MRFv3.m
@@ -1,16 +1,19 @@
-Images.Image_normalized_dicom=0*Images.Images_dicom_rescaled;
+function Images = fp_normalization_MRFv3(Images)  
 
-for i=1:Images.Nx
-    for j=1:Images.Ny
-        Images.norme_dicom(i,j)=norm(squeeze(Images.Images_dicom_rescaled(i,j,2:Images.Nimages)));
-        if Images.norme_dicom(i,j)~=0
-        Images.Image_normalized_dicom(i,j,:)=Images.Images_dicom_rescaled(i,j,:)/Images.norme_dicom(i,j);
+% Images.Image_normalized_dicom=0*Images.Images_dicom_rescaled;
+Images.Image_normalized_dicom = zeros(Images.nX, Images.nY, Images.nImages);
+Images.norme_dicom = zeros(Images.nX, Images.nY);
+
+for i=1:Images.nX
+    for j=1:Images.nY
+%         Images.norme_dicom(i,j)=norm(squeeze(Images.Images_dicom_rescaled(i,j,2:Images.nImages)));
+        Images.norme_dicom(i,j) = norm(squeeze(Images.Images_dicom_rescaled(i,j,:)));
+        if Images.norme_dicom(i,j) ~= 0
+            Images.Image_normalized_dicom(i,j,:) = Images.Images_dicom_rescaled(i,j,:)/Images.norme_dicom(i,j);
         end
     end
 end
 
-
-clear i j
 
 % 
 % Tube_normalized=0*Tube;
