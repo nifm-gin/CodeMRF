f4d1480c4aa7e3ae315c92ece04ff280f8738677
Aurélien <aurelien.delphin@gmail.com>
2020-03-26 14:09:20 +0100

    Minor changes in text files

--- RotCreationTest.m
+++ RotCreationTest.m
@@ -3,16 +3,17 @@ clear all
 PulseProfile.PulseShape = 'Calculated';
 
 % Loads properties such as T1, T2, B1Rel, df and Vlist
-load('/home/aurelien/Installations/CodeMRF/SequencesAndProperties/struct_properties_AD_Test.mat');
+load('/home/aurelien/Installations/CodeMRF/SequencesAndProperties/prop_ad1000Calculated_v10_ph0_Calculated.mat');
 
 % Create sequence parameters on 1000 pulses
-Sequence.nPulses = 1000;
-[FA, TE, TR, m0, Ncycles] = gensequence_mrfv3(Sequence.nPulses);
-Sequence.FA = FA;
-Sequence.TE = TE;
-Sequence.TR = TR;
-Sequence.m0 = m0;
-Sequence.Ncycles = Ncycles;
+load('/home/aurelien/Installations/CodeMRF/SequencesAndProperties/seq_ad1000Calculated_v10_ph0_Calculated.mat');
+% Sequence.nPulses = 1000;
+% [FA, TE, TR, m0, Ncycles] = gensequence_mrfv3(Sequence.nPulses);
+% Sequence.FA = FA;
+% Sequence.TE = TE;
+% Sequence.TR = TR;
+% Sequence.m0 = m0;
+% Sequence.Ncycles = Ncycles;
 
 % Compute pulse profile
 % flow_velocity = 0; 
@@ -34,6 +35,6 @@ dflist = Properties.df;
 flow_velocities = Properties.vlist;
 Nspins = length(positions_mm);
 
-save('/home/aurelien/Installations/CodeMRF/DictionaryCreation/PulseProfiles/PulseDictionaries/dico_Test_07_01_2020.mat', ...
+save('/home/aurelien/Installations/CodeMRF/DictionaryCreation/PulseProfiles/PulseDictionaries/PP_exp_05_03_2020.mat', ...
     'Rot', 'FAlist', 'positions_mm', 'SliceThickness_th_mm', 'p0', 'FA', 'dflist', 'flow_velocities')
 %%
